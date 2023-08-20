2d4e1c276a99168c9fd7cf26aefb40c119fbb8d2
yoonui <yoony0612@ajou.ac.kr>
2023-08-20 20:25:26 +0900

    ADD: 파일 정리 및 router 세팅

---
+++ src/components/basic/Layout.tsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Footer from "./Footer";
+const Layout = (props: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <Header />
+      <main>{props.children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
