b3b4cb53fee52dcc8ee18b4a5244ded43a5329f2
yoonui <yoony0612@ajou.ac.kr>
2023-08-28 21:54:30 +0900

    ADD: footer

--- src/components/basic/Footer.tsx
+++ src/components/basic/Footer.tsx
@@ -1,5 +1,14 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="w-[100%] bg-custom-mint-drak flex justify-center">
+      <div className="w-[70%] text-custom-white text-center my-4">
+        <div>해당 사이트는 '가상 아이돌'의 공식 홈페이지로,</div>
+        <div>실제 존재하는 아이돌과는 연관이 없음을 밝힙니다.</div>
+        <br />
+        <div>@yoony0612</div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
