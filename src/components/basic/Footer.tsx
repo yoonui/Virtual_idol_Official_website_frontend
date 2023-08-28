0bf64b9f469621848dc011e635f3be74ae52974d
yoonui <yoony0612@ajou.ac.kr>
2023-08-28 23:06:17 +0900

    ADD: 홈 화면의 이미지 구현 중

--- src/components/basic/Footer.tsx
+++ src/components/basic/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <div>해당 사이트는 '가상 아이돌'의 공식 홈페이지로,</div>
         <div>실제 존재하는 아이돌과는 연관이 없음을 밝힙니다.</div>
         <br />
-        <div>@yoony0612</div>
+        <div>@YOONUI</div>
       </div>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,37 @@
+import test1 from "../image/test1.jpg";
+import test2 from "../image/test2.jpg";
+import test3 from "../image/test3.jpg";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="w-[100%] px-20 py-10 flex justify-center">
+      <div className="flex overflow-hidden">
+        <div className="w-[300px] h-[500px] overflow-hidden">
+          <img
+            alt="test1"
+            className="w-[300px] h-[500px] transition-all duration-150 hover:transform hover:scale-150"
+            src={test1}
+          />
+        </div>
+
+        <div className="w-[300px] h-[500px] overflow-hidden">
+          <img
+            alt="test2"
+            className="w-[300px] h-[500px] transition-all duration-150 hover:transform hover:scale-150"
+            src={test2}
+          />
+        </div>
+
+        <div className="w-[300px] h-[500px] overflow-hidden">
+          <img
+            alt="test3"
+            className="w-[300px] h-[500px] transition-all duration-150 hover:transform hover:scale-150"
+            src={test3}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
