21f880a7946e254f5e15cb92d375cdb5b0ad29a3
yoonui <yoony0612@ajou.ac.kr>
2023-09-05 22:31:22 +0900

    UPDATE: 대표 색상 수정

--- src/components/basic/Header.tsx
+++ src/components/basic/Header.tsx
@@ -1,37 +1,37 @@
 const Header = () => {
   return (
-    <div className="w-[100%] bg-custom-gray text-custom-white justify-center flex items-center">
+    <div className="w-[100%] bg-symbolic-color-black text-custom-white justify-center flex items-center">
       <div className="w-[70%]">
         <div className="flex justify-end my-4">
           <button className="mr-4 font-bold hover:text-custom-white-dark">
             로그인
           </button>
-          <button className="px-2 py-1 font-bold border-2 rounded border-custom-mint text-custom-mint hover:border-custom-mint-dark hover:text-custom-mint-dark">
+          <button className="px-2 py-1 font-bold border-2 rounded border-symbolic-color-blue text-symbolic-color-blue hover:border-symbolic-color-blue-dark hover:text-symbolic-color-blue-dark">
             회원가입
           </button>
         </div>
-        <div className="my-10 text-center"> 여기에 로고가 들어갑니다 </div>
+        <div className="my-10 text-center">여기에 아이돌 로고가 들어갑니다</div>
 
         <div className="flex justify-between text-2xl font-bold">
           <button className="relative p-4 group">
             <span>HOME</span>
-            <span className="absolute left-0 w-0 h-2 transition-all -bottom-0 bg-custom-mint group-hover:w-full"></span>
+            <span className="absolute left-0 w-0 h-2 transition-all -bottom-0 bg-symbolic-color-blue group-hover:w-full"></span>
           </button>
           <button className="relative p-4 group">
             <span>PROFILE</span>
-            <span className="absolute left-0 w-0 h-2 transition-all -bottom-0 bg-custom-mint group-hover:w-full"></span>
+            <span className="absolute left-0 w-0 h-2 transition-all -bottom-0 bg-symbolic-color-blue group-hover:w-full"></span>
           </button>
           <button className="relative p-4 group">
             <span>GALLERY</span>
-            <span className="absolute left-0 w-0 h-2 transition-all -bottom-0 bg-custom-mint group-hover:w-full"></span>
+            <span className="absolute left-0 w-0 h-2 transition-all -bottom-0 bg-symbolic-color-blue group-hover:w-full"></span>
           </button>
           <button className="relative p-4 group">
             <span>ALBUM</span>
-            <span className="absolute left-0 w-0 h-2 transition-all -bottom-0 bg-custom-mint group-hover:w-full"></span>
+            <span className="absolute left-0 w-0 h-2 transition-all -bottom-0 bg-symbolic-color-blue group-hover:w-full"></span>
           </button>
           <button className="relative p-4 group">
             <span>BOARD</span>
-            <span className="absolute left-0 w-0 h-2 transition-all -bottom-0 bg-custom-mint group-hover:w-full"></span>
+            <span className="absolute left-0 w-0 h-2 transition-all -bottom-0 bg-symbolic-color-blue group-hover:w-full"></span>
           </button>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,24 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     colors: {
-      "custom-black": "#222831",
+      "symbolic-color": "#CCDEE5",
 
-      "custom-gray": "#393E46",
-      "custom-gray-light": "#5e6673",
+      "symbolic-color-black": "#5B6366",
 
-      "custom-mint": "#00ADB5",
-      "custom-mint-dark": "#01868c",
+      "symbolic-color-blue": "#8ACFEA",
+      "symbolic-color-blue-dark": "#8ACFEA",
+
+      "symbolic-color-darkgray": "#3C5A66",
+
+      "symbolic-color-gray": "#9FADB3",
+
+      // "custom-black": "#222831",
+
+      // "custom-gray": "#393E46",
+      // "custom-gray-light": "#5e6673",
+
+      // "custom-mint": "#00ADB5",
+      // "custom-mint-dark": "#01868c",
 
       "custom-white": "#EEEEEE",
       "custom-white-dark": "#bdbdbd",
