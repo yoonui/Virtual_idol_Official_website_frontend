7fe5e9eadd27f2923889c96408af2f28c5bc7f16
yoonui <yoony0612@ajou.ac.kr>
2023-09-12 23:14:12 +0900

    UPDATE: 색상 수정

--- src/components/basic/Header.tsx
+++ src/components/basic/Header.tsx
@@ -3,7 +3,7 @@ const Header = () => {
     <div className="w-[100%] bg-symbolic-color text-symbolic-color-black justify-center flex items-center">
       <div className="w-[70%]">
         <div className="flex justify-end my-4">
-          <button className="mr-4 font-bold text-symbolic-color-black-light hover:text-symbolic-color-black">
+          <button className="mr-4 font-bold hover:text-symbolic-color-black-dark hover:underline">
             로그인
           </button>
           <button className="px-2 py-1 font-bold border-2 rounded border-symbolic-color-darkgray-light text-symbolic-color-darkgray-light hover:border-symbolic-color-darkgray hover:text-symbolic-color-darkgray">
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,13 @@ module.exports = {
       "symbolic-color": "#CCDEE5",
 
       "symbolic-color-black": "#5B6366",
-      "symbolic-color-black-light": "#9ba7ab",
+      "symbolic-color-black-light": "#919ca1",
+      "symbolic-color-black-dark": "#292d2e",
 
       "symbolic-color-blue": "#8ACFEA",
 
       "symbolic-color-darkgray": "#3C5A66",
-      "symbolic-color-darkgray-light": "#6597ab",
+      "symbolic-color-darkgray-light": "#5d8da1",
 
       "symbolic-color-gray": "#9FADB3",
 
